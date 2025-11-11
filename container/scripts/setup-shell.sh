c38d4ce92cded01074773c15a6b543afc76a953c
Mountain Star <github@mtnstar.net>
2025-11-11 14:59:09 +0100

    Fix shell prompt, remove username from prompt

--- container/scripts/setup-shell.sh
+++ container/scripts/setup-shell.sh
@@ -11,11 +11,11 @@ __wnd_git_branch() {
   git rev-parse --is-inside-work-tree >/dev/null 2>&1 || return
   branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
   if [ -n "$branch" ]; then
-    printf " \[\e[0;35m\](%s)\[\e[0m\]" "$branch"
+    printf " \e[0;35m(%s)\e[0m" "$branch"
   fi
 }
 BASH
 
 cat >> /etc/bash.bashrc <<'BASH'
-export PS1='\[\e[1;37m\]W\[\e[1;31m\]N\[\e[1;37m\]D\[\e[0m\]$(__wnd_git_branch) \[\e[1;37m\]\u:\[\e[1;33m\]\w\[\e[0m\]\$ '
+export PS1='\[\e[1;37m\]W\[\e[1;31m\]N\[\e[1;37m\]D\[\e[0m\]$(__wnd_git_branch) \e[1;34m\]\w\[\e[0m\]\$ '
 BASH
