98155f19ab843fed50f6d4c155bc2fa2c7471316
Mountain Star <github@mtnstar.net>
2025-11-25 14:42:27 +0100

    Add freeradius utils

--- container/scripts/install-packages.sh
+++ container/scripts/install-packages.sh
@@ -16,6 +16,7 @@ apt-get install -y --no-install-recommends \
   git git-lfs \
   dnsutils mtr iputils-ping ncat \
   podman slirp4netns fuse-overlayfs uidmap \
+  freeradius-utils \
   bash-completion \
   ssh \
   sudo \
